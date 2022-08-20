12ab59bd05eed28a060a7567a0e8688420a691f9
pantocrator77 <lorenzo.vecchia@gmail.com>
2022-08-20 16:30:02 +0200

    build added git

---
+++ build/precache-manifest.5eff5c39145f3610de08be63c864dd21.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "20ab5cc5a3a8428c64ba",
+    "url": "/static/js/0.dfc8f731.chunk.js"
+  },
+  {
+    "revision": "58222a48eaafd4df6494",
+    "url": "/static/js/1.7cb47bab.chunk.js"
+  },
+  {
+    "revision": "dbf6ad3e518164f7c33b",
+    "url": "/static/js/main.df2332a7.chunk.js"
+  },
+  {
+    "revision": "be7e4dcd99ba319a95b0",
+    "url": "/static/js/runtime~main.760bb09d.js"
+  },
+  {
+    "revision": "64689970bc36653b31f4",
+    "url": "/static/js/4.85834460.chunk.js"
+  },
+  {
+    "revision": "90d2984a0d11f911e99b",
+    "url": "/static/js/5.b3208c38.chunk.js"
+  },
+  {
+    "revision": "71d7ff0186ee8a695d39",
+    "url": "/static/js/6.91f61f44.chunk.js"
+  },
+  {
+    "revision": "c8f35398033907d5b003",
+    "url": "/static/css/7.aea08863.chunk.css"
+  },
+  {
+    "revision": "c8f35398033907d5b003",
+    "url": "/static/js/7.ee3e6a49.chunk.js"
+  },
+  {
+    "revision": "bfbb1869bb0d3d5409c9",
+    "url": "/static/css/8.a94d80a7.chunk.css"
+  },
+  {
+    "revision": "bfbb1869bb0d3d5409c9",
+    "url": "/static/js/8.286534b6.chunk.js"
+  },
+  {
+    "revision": "2ca8379a7d38830c380a",
+    "url": "/static/js/9.80dacf67.chunk.js"
+  },
+  {
+    "revision": "74c1bdaf35648027cf9c",
+    "url": "/static/js/10.86e44d91.chunk.js"
+  },
+  {
+    "revision": "668ef473a5b6fa4eb2ca",
+    "url": "/static/js/11.58999462.chunk.js"
+  },
+  {
+    "revision": "4b631f753469772a954f",
+    "url": "/static/js/12.ffda9f64.chunk.js"
+  },
+  {
+    "revision": "7246fc779191ecc041a0",
+    "url": "/static/js/13.746eac89.chunk.js"
+  },
+  {
+    "revision": "d5423d96815e42896ede",
+    "url": "/static/js/14.1dea0b8a.chunk.js"
+  },
+  {
+    "revision": "24e5fd093b1d03ff9631",
+    "url": "/static/js/15.844f9620.chunk.js"
+  },
+  {
+    "revision": "cb8377b60ed457a3cbe1",
+    "url": "/static/js/16.6ff16283.chunk.js"
+  },
+  {
+    "revision": "a46354673bea53d2e393",
+    "url": "/static/js/17.4117dbd8.chunk.js"
+  },
+  {
+    "revision": "9183952551693b7f7940",
+    "url": "/static/js/18.10f998c6.chunk.js"
+  },
+  {
+    "revision": "282cc4e89d8a5cd02b79",
+    "url": "/static/js/19.17767323.chunk.js"
+  },
+  {
+    "revision": "d87548a90d7bcdd3dce3",
+    "url": "/static/js/20.47f729ff.chunk.js"
+  },
+  {
+    "revision": "91349b0277f6000b80b664851c0d6cc8",
+    "url": "/index.html"
+  }
+];
